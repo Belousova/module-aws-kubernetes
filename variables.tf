f0d095c94a35603f3ea90a22a2ea2fc365fa3368
Tatiana Belousova <t.belousova@gmail.com>
2021-02-26 22:36:30 -0600

    =typo

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "aws_region" {
-  description = "AWS region ID for deployment (e.g. eu-west-1)"
-  type        = string
-  default     = "eu-west-2"
+  type    = string
+  default = "eu-west-2"
 }
 
 variable "env_name" {
